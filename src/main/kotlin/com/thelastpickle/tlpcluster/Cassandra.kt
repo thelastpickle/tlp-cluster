f8382cd3d03488bae689752c1032cb7cfad57c62
Jon Haddad <jon@jonhaddad.com>
2019-02-15 10:37:40 -0800

    note for later

--- src/main/kotlin/com/thelastpickle/tlpcluster/Cassandra.kt
+++ src/main/kotlin/com/thelastpickle/tlpcluster/Cassandra.kt
@@ -47,4 +47,12 @@ class Cassandra(val gitLocation: File) {
     fun listBuilds() : List<String> {
         return buildDir.listFiles().filter { it.isDirectory }.map { it.name }
     }
+
+    fun pullVersion(version: String) {
+        // http://dl.bintray.com/apache/cassandra/pool/main/c/cassandra/
+        // example http://dl.bintray.com/apache/cassandra/pool/main/c/cassandra/cassandra_2.1.14_all.deb
+        // org.apache.commons.io.FileUtils.copyURLToFile(URL, File
+
+    }
+
 }
