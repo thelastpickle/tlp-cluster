5a49389c53471087e763a3353e655da10729eab6
Jon Haddad <jon@jonhaddad.com>
2019-02-15 11:45:20 -0800

    downloading deb file and creating conf

--- src/main/kotlin/com/thelastpickle/tlpcluster/CassandraDebUnpacker.kt
+++ src/main/kotlin/com/thelastpickle/tlpcluster/CassandraDebUnpacker.kt
@@ -3,22 +3,26 @@ package com.thelastpickle.tlpcluster
 import org.apache.commons.io.FileUtils
 import java.io.File
 import java.net.URL
+import java.nio.file.Files
 import java.nio.file.Path
 
 /**
  * Utility class for fetching and extracting deb packages
  * Manipulates the docker image behind the scenes for deb extraction
  *
+ *  Caches files locally
+ *
  */
 class CassandraDebUnpacker(val version: String, val dest: Path) {
 
-    fun download(version: String) {
+    fun download() {
         // example http://dl.bintray.com/apache/cassandra/pool/main/c/cassandra/cassandra_2.1.14_all.deb
 
-        FileUtils.copyURLToFile(URL(getURL(version)), File(dest.toFile(), getFileName(version)))
+        FileUtils.copyURLToFile(URL(getURL()), File(dest.toFile(), getFileName()))
+        File(dest.toFile(), "conf").mkdir()
 
     }
 
-    fun getURL(version: String) = "http://dl.bintray.com/apache/cassandra/pool/main/c/cassandra/" + getFileName(version)
-    fun getFileName(version: String) = "cassandra_${version}_all.deb"
+    fun getURL() = "http://dl.bintray.com/apache/cassandra/pool/main/c/cassandra/" + getFileName()
+    fun getFileName() = "cassandra_${version}_all.deb"
 }
--- src/test/kotlin/com/thelastpickle/tlpcluster/CassandraDebUnpackerTest.kt
+++ src/test/kotlin/com/thelastpickle/tlpcluster/CassandraDebUnpackerTest.kt
@@ -1,26 +1,45 @@
 package com.thelastpickle.tlpcluster
 
+import org.apache.commons.io.FileUtils
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import java.io.File
+import java.nio.file.Files
 import java.nio.file.Path
 
 internal class CassandraDebUnpackerTest {
 
     lateinit var downloadDir : Path
+    lateinit var unpacker : CassandraDebUnpacker
 
     @BeforeEach
-    fun createTestDirectories() {
+    fun setupUnpacker() {
+        downloadDir = Files.createTempDirectory("test")
+        unpacker = CassandraDebUnpacker("2.1.14", downloadDir)
 
+    }
+    @AfterEach
+    fun tearDownUnpacker() {
+        FileUtils.deleteDirectory(downloadDir.toFile())
     }
 
     @Test
-    fun download() {
+    fun ensureDownloadCreatesDebPackageAndConfFiles() {
+        unpacker.download()
+
+        assertThat(File(downloadDir.toFile(), "cassandra_2.1.14_all.deb")).exists()
+        assertThat(File(downloadDir.toFile(), "conf")).exists()
+
     }
 
     @Test
     fun getURL() {
+        val expected = "http://dl.bintray.com/apache/cassandra/pool/main/c/cassandra/cassandra_2.1.14_all.deb"
+        assertThat(unpacker.getURL()).isEqualTo(expected)
     }
 
     @Test
