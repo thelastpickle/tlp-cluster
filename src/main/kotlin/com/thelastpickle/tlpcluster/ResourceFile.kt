1280ba9a721100b99aa84703120efdfb6988d00a
Jon Haddad <jon@jonhaddad.com>
2019-02-15 14:22:04 -0800

    added resource file which will be useful for creating docker containers with shell scripts in them

---
+++ src/main/kotlin/com/thelastpickle/tlpcluster/ResourceFile.kt
@@ -0,0 +1,24 @@
+package com.thelastpickle.tlpcluster
+
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FilenameUtils
+import org.apache.commons.io.IOUtils
+import java.io.File
+import java.net.URL
+
+/**
+ * Creates a temporary file from a resource location
+ */
+class ResourceFile(val resource: URL) {
+
+    val fp: File
+
+    init {
+        fp = File.createTempFile(
+                FilenameUtils.getBaseName(resource.file),
+                FilenameUtils.getExtension(resource.file))
+
+        IOUtils.copy(resource.openStream(), FileUtils.openOutputStream(fp))
+
+    }
+}
