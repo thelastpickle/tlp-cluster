2dac62668fb584c65cdf52d027a81cfd4936b923
Jon Haddad <jon@jonhaddad.com>
2019-02-20 11:11:12 -0800

    more logging around hosts

--- src/main/kotlin/com/thelastpickle/tlpcluster/configuration/TFState.kt
+++ src/main/kotlin/com/thelastpickle/tlpcluster/configuration/TFState.kt
@@ -48,8 +48,9 @@ class TFState(val context: Context,
                 val tmp = hostRegex.find(serverName)!!.groups
 
                 log.debug { "Regex find: $tmp" }
-
-                result.add(Host(public!!, private!!, tmp[1]?.value.toString() + tmp[2]?.value.toString()))
+                val host = Host(public!!, private!!, tmp[1]?.value.toString() + tmp[2]?.value.toString())
+                log.info { "Adding host: $host" }
+                result.add(host)
             }
 
         }
