98c3f43e4b409a920c7a9a41d16d4f5e8eafdc32
Jon Haddad <jon@jonhaddad.com>
2019-02-23 20:51:48 -0800

    working on integration tests

---
+++ src/integration-test/kotlin/com/thelastpickle/tlpcluster/MainTest.kt
@@ -0,0 +1,21 @@
+package com.thelastpickle.tlpcluster
+
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+
+
+    @Test
+    fun basicTest() {
+        main(arrayOf("init", "test", "test", "test"))
+        main(arrayOf("up",  "--yes"))
+        main(arrayOf("start"))
+        main(arrayOf("use", "3.11.4"))
+    }
+
+
+    fun init() {
+
+    }
+}
