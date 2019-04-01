c8feade7f3b994eb49bd29903e0db417c688071d
Jon Haddad <jon@jonhaddad.com>
2019-04-01 11:51:26 -0700

    fixed bug with config test

--- src/test/kotlin/com/thelastpickle/tlpcluster/terraform/ConfigurationTest.kt
+++ src/test/kotlin/com/thelastpickle/tlpcluster/terraform/ConfigurationTest.kt
@@ -7,12 +7,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class ConfigurationTest {
-    val tags = mutableMapOf("ticket" to "TEST-1",
-            "client" to "TEST CLIENT",
-            "purpose" to "Testing tags",
-            "email" to "test@test.com")
 
-    private val c = Configuration(tags, "us-west-2", context = Context.testContext())
+    private val c = Configuration("TEST-1", "TEST CLIENT", "Testing tags", "us-west-2", context = Context.testContext())
 
     @Test
     fun ensureSetVariableWorks() {
