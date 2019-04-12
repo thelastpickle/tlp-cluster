020cbf7bbdd32ff06917f767c38087bd697cd925
Jon Haddad <jon@jonhaddad.com>
2019-04-11 22:09:07 -0700

    pulling in imported data to generate the final yaml doc

--- src/main/kotlin/com/thelastpickle/tlpcluster/instances/Regions.kt
+++ src/main/kotlin/com/thelastpickle/tlpcluster/instances/Regions.kt
@@ -3,6 +3,9 @@ package com.thelastpickle.tlpcluster.instances
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.thelastpickle.tlpcluster.YamlDelegate
+import com.thelastpickle.tlpcluster.instances.importers.InstancesImporter
+import com.thelastpickle.tlpcluster.instances.importers.RegionImporter
+import com.thelastpickle.tlpcluster.instances.importers.UbuntuImporter
 import org.apache.logging.log4j.kotlin.logger
 
 
@@ -20,11 +23,21 @@ data class Regions(val regions: Map<String, Region>) {
     companion object {
         val log = logger()
         val yaml : ObjectMapper by YamlDelegate()
+        val url = this::class.java.getResource("regions.yaml")!!
 
         fun load() : Regions {
-            val url = this::class.java.getResource("regions.yaml")
             val regions = yaml.readValue<Regions>(url)
             return regions
+        }
+
+        /**
+         * Regenerates the local file
+         */
+        fun regenerate() {
+            val regionImporter = RegionImporter.loadFromJson()
+            val ubuntu = UbuntuImporter.loadFromResource()
+            val instances = InstancesImporter.loadFromCSV()
+
         }
 
 
