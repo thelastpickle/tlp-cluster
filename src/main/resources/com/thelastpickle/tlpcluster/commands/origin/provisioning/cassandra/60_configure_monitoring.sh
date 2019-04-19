3c7ad203c09f8117d6d36b3c678b12d2f729907d
Jon Haddad <jon@jonhaddad.com>
2019-04-19 09:08:53 -0700

    added the node exporter

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/60_configure_monitoring.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/60_configure_monitoring.sh
@@ -11,5 +11,36 @@ curl -LO https://github.com/instaclustr/cassandra-exporter/releases/download/v${
 sudo mv ${EXPORTER_AGENT_JAR} ${CASSANDRA_LIB}/
 
 echo "JVM_OPTS=\"\$JVM_OPTS -javaagent:${CASSANDRA_LIB}/${EXPORTER_AGENT_JAR}\"" | sudo tee -a /etc/cassandra/cassandra-env.sh
+echo "Finished configuring Cassandra monitoring for the Cassandra process"
 
-echo "Finished configuring Cassandra monitoring"
+echo "Grabbing the node exporter for system metrics"
+
+sudo useradd --no-create-home --shell /bin/false node_exporter
+
+wget https://github.com/prometheus/node_exporter/releases/download/v0.17.0/node_exporter-0.17.0.linux-amd64.tar.gz
+
+tar zxvf node_exporter*
+sudo cp node_exporter*/node_exporter /usr/local/bin/
+
+cat <<EOF | sudo tee /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl status node_exporter
+
+echo "Finished setting up node exporter"
