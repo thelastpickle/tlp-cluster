244c65b43008ee82cd94206ec7646e88b9545241
Jon Haddad <jon@jonhaddad.com>
2019-04-19 12:06:51 -0700

    Ninja fix to order of flame graph tool

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/70_flamegraphs.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/70_flamegraphs.sh
@@ -4,11 +4,12 @@
 sudo bash -c "echo 1 > /proc/sys/kernel/perf_event_paranoid"
 sudo bash -c "echo 0 > /proc/sys/kernel/kptr_restrict"
 
+mkdir profiler
+cd profiler
 
 wget https://github.com/jvm-profiling-tools/async-profiler/releases/download/v1.5/async-profiler-1.5-linux-x64.tar.gz
 
-mkdir profiler
-cd profiler
+
 
 tar -zxvf async-profiler*
 
