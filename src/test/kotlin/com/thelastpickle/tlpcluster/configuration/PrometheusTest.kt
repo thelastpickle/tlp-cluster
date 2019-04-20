b08a51c08712da321ef42d626776c8da52bfcb3d
Jon Haddad <jon@jonhaddad.com>
2019-04-20 13:12:15 -0700

    adding tests

--- src/test/kotlin/com/thelastpickle/tlpcluster/configuration/PrometheusTest.kt
+++ src/test/kotlin/com/thelastpickle/tlpcluster/configuration/PrometheusTest.kt
@@ -7,26 +7,35 @@ import org.junit.jupiter.api.Test
 
 internal class PrometheusTest {
 
-    @Test
-    fun testDSLBuilder() {
-        val prometheus = prometheus {
-            global {
-                scape_interval = "15s"
-            }
-            scrape_config {
-                job_name = "prometheus"
-                scape_interval = "5s"
+    val config = prometheus {
+        global {
+            scape_interval = "15s"
+        }
+        scrape_config {
+            job_name = "prometheus"
+            scape_interval = "5s"
 
-                static_config {
-                    targets {
-                        +"127.0.0.1:8000"
-                    }
-
+            static_config {
+                targets {
+                    +"127.0.0.1:8000"
                 }
 
             }
 
         }
+
+    }
+
+
+    @Test
+    fun testGlobalConfig() {
+        assertThat(config.global.scape_interval).isEqualTo("15s")
+    }
+
+    @Test
+    fun testScrapeConfigs() {
+        val scrape = config.scrape_configs
+        assertThat(scrape).isNotEmpty()
     }
 
 
