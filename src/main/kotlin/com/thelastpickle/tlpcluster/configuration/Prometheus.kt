95a464b0a8a2b421f353d17082e38518b5e7fd06
Jon Haddad <jon@jonhaddad.com>
2019-04-20 15:30:14 -0700

    working on verifying the yaml against prometheus

--- src/main/kotlin/com/thelastpickle/tlpcluster/configuration/Prometheus.kt
+++ src/main/kotlin/com/thelastpickle/tlpcluster/configuration/Prometheus.kt
@@ -1,6 +1,8 @@
 package com.thelastpickle.tlpcluster.configuration
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.thelastpickle.tlpcluster.YamlDelegate
+import org.yaml.snakeyaml.Yaml
 import java.io.File
 import java.io.OutputStream
 
@@ -9,7 +11,7 @@ import java.io.OutputStream
 class Prometheus(var global: Global = Global("15s"),
                  var scrape_configs: MutableList<ScrapeConfig> = mutableListOf()) {
 
-    class Global(var scape_interval: String = "")
+    class Global(@JsonProperty("scrape_intervals") var scape_interval: String = "")
 
     class ScrapeConfig(var job_name: String = "",
                        var scape_interval: String = "",
@@ -37,6 +39,9 @@ class Prometheus(var global: Global = Global("15s"),
     }
 
     companion object {
+
+        val yaml by YamlDelegate()
+
         fun writeConfiguration(cassandra: List<String>, stress: List<String>, out: OutputStream) {
 
             // TODO: Move out of here and make it more testable
@@ -61,32 +66,9 @@ class Prometheus(var global: Global = Global("15s"),
 
 
             }
-
-
-            yaml.writeValue(file, prometheus)
-            val prometheus = prometheus {
-                scrape_config {
-                    job_name = "prometheus"
-
-                    static_config {
-                        job_name = "prometheus"
-                        targets = listOf("localhost:9090")
-                    }
-                    static_config {
-                        job_name = "cassandra"
-                        targets = cassandra.map { "$it:9500" }
-
-                    }
-                    static_config {
-                        job_name = "stress"
-                        targets = stress.map { "$it:9501" }
-                    }
-                }
-            }
-
+            yaml.writeValue(out, config)
         }
     }
-
 }
 
 fun prometheus(block: Prometheus.() -> Unit) : Prometheus {
@@ -102,6 +84,12 @@ fun prometheus(block: Prometheus.() -> Unit) : Prometheus {
  */
 fun main() {
 
+    val c = listOf("192.168.1.1", "192.168.1.2")
+    val s = listOf("192.168.2.1", "192.168.2.2")
+    val out = File("prometheus.yml").outputStream()
+
+
+    Prometheus.writeConfiguration(c, s, out)
 }
 
 
