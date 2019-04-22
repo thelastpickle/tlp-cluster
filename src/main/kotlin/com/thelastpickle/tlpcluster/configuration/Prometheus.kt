06611fa5ff139c47e37c724f31fc00e853303866
Jon Haddad <jon@jonhaddad.com>
2019-04-22 10:49:34 -0700

    fixed static configs

--- src/main/kotlin/com/thelastpickle/tlpcluster/configuration/Prometheus.kt
+++ src/main/kotlin/com/thelastpickle/tlpcluster/configuration/Prometheus.kt
@@ -6,12 +6,21 @@ import com.thelastpickle.tlpcluster.YamlDelegate
 import java.io.File
 import java.io.OutputStream
 
-// TODO: This needs some additional options
-// Also need to validate the config generated here is valid prometheus config
+/**
+ * This is the top level object that holds all prometheus server configuration
+ * Global is a normal scrape config
+ * Generally speaking you only need to worry about making changes to the config in
+ * Prometheus.writeConfiguration
+ *
+ * The rest is simple classes that will get taken for you automatically
+ */
 class Prometheus(var global: ScrapeConfig = ScrapeConfig(scrape_interval = "15s"),
                  var scrape_configs: MutableList<ScrapeConfig> = mutableListOf()) {
 
-
+    /**
+     * You will never need to call this directly
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#scrape_config">Prometheus Scrape Config</a>
+     */
     class ScrapeConfig(@JsonInclude(JsonInclude.Include.NON_EMPTY) var job_name: String = "",
                        @JsonInclude(JsonInclude.Include.NON_NULL)
                        var scrape_interval: String? = null,
@@ -26,14 +35,31 @@ class Prometheus(var global: ScrapeConfig = ScrapeConfig(scrape_interval = "15s"
         }
     }
 
-    // belongs to scrape config
+    /**
+     * belongs to scrape config.  We're only using the targets list for now, so I haven't included anything else here
+     * The only other thing we could use is labels, and that doesn't appear to be applicable here
+     *
+     * @see <a href="https://prometheus.io/docs/prometheus/latest/configuration/configuration/#static_config">Static Config</a>
+     *
+     */
     class StaticConfig(var targets: List<String> = listOf())
 
-
+    /**
+     * Used as Prometheus.global { }
+     *
+     * @param block Lambda that will be applied to the new ScapeConfig.
+     */
     fun global(block: ScrapeConfig.() -> Unit) {
         global = ScrapeConfig().apply(block)
     }
 
+    /**
+     * Used as Prometheus.global {}
+     *
+     * @param block Lambda that will be applied to the new ScapeConfig.
+     *
+     * @see com.thelastpickle.tlpcluster.configuration.PrometheusTest for usage
+     */
     fun scrape_config(block: ScrapeConfig.() -> Unit) {
         val config = ScrapeConfig().apply(block)
         scrape_configs.add(config)
@@ -50,18 +76,20 @@ class Prometheus(var global: ScrapeConfig = ScrapeConfig(scrape_interval = "15s"
                     job_name = "prometheus"
 
                     static_config {
-                        job_name = "prometheus"
                         targets = listOf("localhost:9090")
                     }
+                }
+                scrape_config {
+                    job_name = "cassandra"
 
                     static_config {
-                        job_name = "cassandra"
                         targets = cassandra.map { "$it:9500" }
                     }
-
+                }
+                scrape_config {
+                    job_name = "stress"
 
                     static_config {
-                        job_name = "stress"
                         targets = stress.map { "$it:9501" }
                     }
                 }
