d0207e17c71a17744ca8712472afccc57816b8da
Jon Haddad <jon@jonhaddad.com>
2019-05-06 14:29:01 -0700

    removed test containers

--- src/test/kotlin/com/thelastpickle/tlpcluster/containers/AnsibleTest.kt
+++ src/test/kotlin/com/thelastpickle/tlpcluster/containers/AnsibleTest.kt
@@ -2,23 +2,15 @@ package com.thelastpickle.tlpcluster.containers
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.junit.jupiter.Container
-
-class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
 
 
-@Testcontainers
 internal class AnsibleTest {
 
-    @Container
-    var ubuntu = KGenericContainer("ubuntu:bionic")
-            .withExposedPorts(22)
+//    var ubuntu =
 
     @Test
     fun basicAnsibleTest() {
         println("Hello")
-        assertTrue(ubuntu.isRunning)
+//        assertTrue(ubuntu.isRunning)
     }
 }
