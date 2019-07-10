0d3d0b90f89cba4e81e509e720a31ca6e0f8c63a
Jon Haddad <jon@jonhaddad.com>
2019-07-10 10:57:29 -0700

    generating dashboards

--- src/dashboards/entrypoint.sh
+++ src/dashboards/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-echo "hey everyone"
+echo "Generating dashboards"
 
 for fp in $(ls /input); do
-    echo $fp
+    echo "Generating $fp"
+
+    /usr/local/bin/jsonnet -J /grafonnet-lib/ /input/$fp -o /output/${fp%net}
 done
