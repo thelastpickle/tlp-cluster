ed2f7ec60d469b65b9691e2791c190445aa92ad8
Jon Haddad <jon@jonhaddad.com>
2019-07-18 16:09:44 -0700

    trying to get dashboards to work, fun times

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/monitoring/01_install_grafana.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/monitoring/01_install_grafana.sh
@@ -5,7 +5,12 @@ echo "Installing Grafana"
 sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
 sudo wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
 sudo apt-get update
-sudo apt-get install grafanaß
+sudo apt-get install grafana
 
 sudo cp config/grafana/datasource.yaml /etc/grafana/provisioning/datasources/
 sudo chown root:grafana /etc/grafana/provisioning/datasources/datasource.yaml
+
+sudo cp config/grafana/dashboards.yaml /etc/grafana/provisioning/dashboards/
+sudo cp config/grafana/dashboards/* /var/lib/grafana/dashboards/
+
+sudo chown -R grafana /var/lib/grafana/dashboards/
