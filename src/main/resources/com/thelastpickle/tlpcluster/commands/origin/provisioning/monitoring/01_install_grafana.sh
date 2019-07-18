e31af7df55034b68115f14febf5d342d91f60f3f
Jon Haddad <jon@jonhaddad.com>
2019-07-18 14:55:24 -0700

    fixing grafana install

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/monitoring/01_install_grafana.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/monitoring/01_install_grafana.sh
@@ -2,11 +2,10 @@
 
 echo "Installing Grafana"
 
-curl -s https://packagecloud.io/install/repositories/grafana/stable/script.deb.sh | sudo bash
-
-sudo add-apt-repository "deb https://packagecloud.io/grafana/stable/debian/ stretch main"
+sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+sudo wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
 sudo apt-get update
-sudo apt-get install -y --allow-unauthenticated grafana
+sudo apt-get install grafanaß
 
 sudo cp config/grafana/datasource.yaml /etc/grafana/provisioning/datasources/
 sudo chown root:grafana /etc/grafana/provisioning/datasources/datasource.yaml
