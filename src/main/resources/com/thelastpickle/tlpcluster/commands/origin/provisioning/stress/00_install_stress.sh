34a550dfab84f8ec0e78fd8580c6d282c2f973fc
Jon Haddad <jon@jonhaddad.com>
2019-08-07 09:35:55 -0700

    Install tlp-stress from bintray
    tlp-stress was being pulled from S3 since there wasn't a deb repo available.  Now that it's been released as 1.0 we will pull it from the bintray repo.
    
    Closes #120

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/stress/00_install_stress.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/stress/00_install_stress.sh
@@ -2,10 +2,9 @@
 
 echo "Installing tlp-stress"
 
-PACKAGE_VERSION=1.0
-PACKAGE_NAME=tlp-stress_${PACKAGE_VERSION}_amd64.deb
 
-curl -LO https://s3-us-west-2.amazonaws.com/tlp-public/${PACKAGE_NAME}
+echo "deb https://dl.bintray.com/thelastpickle/tlp-tools-deb weezy main" | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 2895100917357435
+sudo apt update
+sudo apt install -y tlp-stress
 
-sudo apt-get update
-sudo apt-get install -y ./${PACKAGE_NAME}
