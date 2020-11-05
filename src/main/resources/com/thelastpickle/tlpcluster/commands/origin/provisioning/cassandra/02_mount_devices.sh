212ea44af553cb3bba85a90fd035a04661ea820d
Alexander Dejanovski <alex@thelastpickle.com>
2020-11-05 11:17:02 +0100

    Fix device mounting for Cassandra instances Fix instance provisioning and disk mounts

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/02_mount_devices.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/02_mount_devices.sh
@@ -5,7 +5,7 @@ echo "Mounting data volume "
 
 MOUNT_POINT=/var/lib/cassandra/
 
-DEVICES=( "/dev/nvme1n1" "/dev/xvdb" )
+DEVICES=( "/dev/nvme0n1" "/dev/nvme1n1" "/dev/xvdb" )
 
 for TARGET_DEVICE in "${DEVICES[@]}"; do
     echo "Checking $TARGET_DEVICE"
@@ -13,10 +13,10 @@ for TARGET_DEVICE in "${DEVICES[@]}"; do
     if [[ -b "$TARGET_DEVICE" ]]
     then
 	    echo "Creating volume from $TARGET_DEVICE"
-      sudo mkfs.xfs -s size=4096 -f /dev/xvdb
+      sudo mkfs.xfs -s size=4096 -f $TARGET_DEVICE
       sudo mkdir -p ${MOUNT_POINT}
 	    sudo mount $TARGET_DEVICE $MOUNT_POINT
-	    sudo blockdev --setra 8 $TARGET_DEVICE
+	    sudo blockdev --setra 64 $TARGET_DEVICE
       sudo chmod 755 ${MOUNT_POINT}
       break 2
     fi
