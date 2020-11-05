a5f3f8342b3413537f15dcdbe5aa51819fd38cdc
Alexander Dejanovski <alex@thelastpickle.com>
2020-11-05 07:32:41 +0100

    Reduce install phase instability

--- src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/20_java.sh
+++ src/main/resources/com/thelastpickle/tlpcluster/commands/origin/provisioning/cassandra/20_java.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set +o pipefail
-set +e
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openjdk-8-dbg || sudo apt-get install -f -y
+curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+jabba install adopt@1.8.0-272
+jabba alias default adopt@1.8.0-272
+sudo update-alternatives --install /usr/bin/java java ${JAVA_HOME%*/}/bin/java 20000
+sudo update-alternatives --install /usr/bin/javac javac ${JAVA_HOME%*/}/bin/javac 20000
