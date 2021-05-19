f0984ecbbf2a677339b2780b10e63830c8b5025c
Radovan Zvoncek <radovan@thelastpickle.com>
2021-05-19 17:15:26 +0300

    Make gradle push to CloudSmith instead of Bintray

--- src/main/kotlin/com/thelastpickle/tlpcluster/Containers.kt
+++ src/main/kotlin/com/thelastpickle/tlpcluster/Containers.kt
@@ -3,7 +3,7 @@ package com.thelastpickle.tlpcluster
 enum class Containers(val containerName: String, val tag: String) {
     PSSH("thelastpickle/pssh", "1.0"),
     CASSANDRA_BUILD("thelastpickle/cassandra-build", "1.0"),
-    TERRAFORM("hashicorp/terraform", "0.11.14");
+    TERRAFORM("hashicorp/terraform", "0.11.15");
 
     val imageWithTag : String
         get() = "$containerName:$tag"
